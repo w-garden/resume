eae501b878ae7cdc810306c359edc73259aaa1c8
w-garden <w.garden316@gmail.com>
2025-08-01 15:25:00 +0900

    commit

---
+++ src/main/java/com/resume/controller/ResumeController.java
@@ -0,0 +1,25 @@
+package com.resume.controller;
+
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Document;
+import com.vladsch.flexmark.util.ast.Node;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@RestController
+public class ResumeController {
+
+    private final Parser parser = Parser.builder().build();
+    private final HtmlRenderer renderer = HtmlRenderer.builder().build();
+
+    @GetMapping("/resume/html")
+    public String getResumeHtml() throws Exception {
+        String markdown = Files.readString(Path.of("resume.md"));
+        Node document = parser.parse(markdown);
+        return renderer.render(document);
+    }
+}
